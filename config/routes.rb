629e64b277cf231179876e761f4204fa882d8c4d
ueda3ban@gmail.com <ec2-user@ip-172-31-16-37.ec2.internal>
2021-10-01 06:24:44 +0000

    [Add]Gem'devise',controllers(homes,contributors,recipients)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :contributors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  
+  devise_for :contributors, controllers: {
+    sessions: 'contributors/sessions',
+    passwords: 'contributors/passwords',
+    registrations: 'contributors/registrations'
+  }
+
+  devise_for :recipients, controllers: {
+    sessions: 'recipients/sessions',
+    passwords: 'recipients/passwords',
+    registrations: 'recipients/registrations'
+  }
 end
