0dda63f536e97038c60d9ee1500828c8d4fbc34c
yanmasa <ueda3ban@gmail.com>
2021-10-07 06:28:16 +0000

    devise login signin

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,13 @@
 class HomesController < ApplicationController
   def top
   end
+  
+  def about
+  end
+  
+  def contributors_top
+  end
+  
+  def recipients_top
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
+  get '/about' => 'homes#about', as: 'about'
+  get '/contributors/homes' => 'homes#contributors_top', as: 'c_top'
+  get '/recipients/homes' => 'homes#recipients_top', as: 'r_top'
   
+
   devise_for :contributors, controllers: {
     sessions: 'contributors/sessions',
     passwords: 'contributors/passwords',
@@ -13,6 +17,7 @@ Rails.application.routes.draw do
     passwords: 'recipients/passwords',
     registrations: 'recipients/registrations'
   }
-  
-  
+
+  resources :contributors
+  resources :recipients
 end
